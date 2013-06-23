684b69c46968aac8ba07bbfaa9cdbe645afc9b6b
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-06-23 16:45:02 -0700

    big ole refactor, new class: P2Estimator

--- lithoxyl/p_squared.py
+++ lithoxyl/p_squared.py
@@ -7,7 +7,7 @@ quantile-related statistics (e.g., median, quartiles).
 For description of the algorithm defined in
 http://www.cs.wustl.edu/~jain/papers/ftp/psqr.pdf
 
-Implemented by Kurt Rose and Mahmoud Hashemi (mostly Kurt Rose).
+Implemented by Kurt Rose and Mahmoud Hashemi.
 
 Copyright 2013, 3-clause BSD License
 """
@@ -139,6 +139,100 @@ class QuantileAccumulator(object):
         return (data[idx_f] * (idx_c - idx)) + (data[idx_c] * (idx - idx_f))
 
 
+class P2Estimator(object):
+    def __init__(self, q_points, data):
+        len_data, len_qp = len(data), len(q_points)
+        len_init = len_qp + 2
+        if len_data < len_init:
+            tmpl = 'expected %d or more data points for %d quantiles (got %d)'
+            raise ValueError(tmpl % (len_init, len_qp, len_data))
+        try:
+            qps = sorted([float(x) for x in set(q_points or [])])
+            if not qps or not all([0 <= x <= 100 for x in qps]):
+                raise ValueError()
+        except:
+            raise ValueError('invalid quantile point(s): %r' % (q_points,))
+        else:
+            # TODO: pop off 0 and 100?
+            self._q_points = qps
+
+        self._count = 0
+        initial = sorted(data[:len_init])
+        self.min = initial[0]
+        self.max = initial[-1]
+        vals = [[i + 2, x] for i, x in enumerate(initial[1:-2])]
+        self._points = zip(self._q_points, vals)  # TODO: marks?
+
+        for i in xrange(len_init, len_data):
+            self.add(data[i])
+
+    def add(self, val):
+        self._count += 1
+
+        if val < self.min:
+            self.min = val
+        elif val > self.max:
+            self.max = val
+        cur_min, cur_max = self.min, self.max
+        count, scale = self._count, self._count - 1
+        points, _nxt = self._points, self._nxt
+
+        # right-most point is stopping case; handle first
+        right = points[-1][1]
+        if val <= right[1]:
+            right[0] += 1
+            if right[0] == count:
+                right[0] -= 1
+        # handle the rest of the points
+        for i in range(len(points) - 2, -1, -1):
+            point = points[i][1]
+            if val <= point[1]:
+                point[0] += 1
+                if point[0] == points[i + 1][1][0]:
+                    point[0] -= 1
+        # left-most point is a special case
+        left = points[0][1]
+        left[1], left[0] = _nxt(1, cur_min, left[0], left[1],
+                                points[1][1][0], points[1][1][1],
+                                points[0][0] / 100.0, scale)
+        # update estimated locations of percentiles
+        for i in range(1, len(points) - 1):
+            prev = points[i - 1][1]
+            point = points[i][1]
+            nxt = points[i + 1][1]
+            point[1], point[0] = _nxt(prev[0], prev[1], point[0],
+                                      point[1], nxt[0], nxt[1],
+                                      points[i][0] / 100.0, scale)
+        # right-most point is a special case
+        right[1], right[0] = _nxt(points[-2][1][0], points[-2][1][1],
+                                  right[0], right[1], count, cur_max,
+                                  points[-1][0] / 100.0, scale)
+
+    def get_quantiles(self):
+        data = dict([(e[0], e[1][1]) for e in self._points])
+        return data
+
+    @staticmethod
+    def _nxt(left_n, left_q, cur_n, cur_q, right_n, right_q, quantile, scale):
+        # calculate desired position
+        d = int(scale * quantile + 1 - cur_n)
+        if d:
+            d = copysign(1, d)  # clamp d at +/- 1
+            if left_n < cur_n + d < right_n:  # try parabolic eqn
+                nxt_q = (cur_q + (d / (right_n - left_n)) *
+                         ((cur_n - left_n + d) * (right_q - cur_q) /
+                          (right_n - cur_n) +
+                          (right_n - cur_n - d) * (cur_q - left_q) /
+                          (cur_n - left_n)))
+                if not (left_q < nxt_q < right_q):  # fall back on linear eqn
+                    if d == 1:
+                        nxt_q = cur_q + (right_q - cur_q) / (right_n - cur_n)
+                    elif d == -1:
+                        nxt_q = cur_q - (left_q - cur_q) / (left_n - cur_n)
+                return nxt_q, cur_n + d
+        return cur_q, cur_n
+
+
 class P2QuantileAccumulator(object):
     """
     TODO
@@ -150,90 +244,7 @@ class P2QuantileAccumulator(object):
     * Off-by-two error? 99.9 and 99.99 aren't being returned
     """
     def __init__(self, percentiles=DEFAULT_PERCENTILES):
-        self.first_n = []
-        self.count = 0
-        self.percentiles = percentiles
-
-    @property
-    def median(self):
-        return self.get_quantiles()[50.0]
-
-    def _start(self):
-        first_n = sorted(self.first_n)
-        self.min = first_n[0]
-        self.max = first_n[-1]
-        vals = [[i + 1, first_n[i]] for i in range(1, len(first_n) - 1)]
-        self.points = zip(self.percentiles, vals)
-
-    def add(self, val):
-        self.count += 1
-
-        if len(self.first_n) < len(self.percentiles) + 2:
-            self.first_n.append(val)
-            if len(self.first_n) == len(self.percentiles):
-                self._start()
-            return
-
-        if val < self.min:
-            self.min = val
-        if val > self.max:
-            self.max = val
-        scale = self.count - 1
-        # right-most point is stopping case; handle first
-        right = self.points[-1][1]
-        if val <= right[1]:
-            right[0] += 1
-            if right[0] == self.count:
-                right[0] -= 1
-        # handle the rest of the points
-        for i in range(len(self.points) - 2, -1, -1):
-            point = self.points[i][1]
-            if val <= point[1]:
-                point[0] += 1
-                if point[0] == self.points[i + 1][1][0]:
-                    point[0] -= 1
-        # left-most point is a special case
-        left = self.points[0][1]
-        left[1], left[0] = _nxt(1, self.min, left[0], left[1],
-                                self.points[1][1][0], self.points[1][1][1],
-                                self.points[0][0] / 100.0, scale)
-        # update estimated locations of percentiles
-        for i in range(1, len(self.points) - 1):
-            prev = self.points[i - 1][1]
-            point = self.points[i][1]
-            nxt = self.points[i + 1][1]
-            point[1], point[0] = _nxt(prev[0], prev[1], point[0],
-                                      point[1], nxt[0], nxt[1],
-                                      self.points[i][0] / 100.0, scale)
-        # right-most point is a special case
-        right[1], right[0] = _nxt(self.points[-2][1][0], self.points[-2][1][1],
-                                  right[0], right[1], self.count, self.max,
-                                  self.points[-1][0] / 100.0, scale)
-
-    def get_quantiles(self):
-        data = dict([(e[0], e[1][1]) for e in self.points])
-        return data
-
-    def __repr__(self):
-        return "Measure(%r)" % (self.get_quantiles(),)
-
-
-def _nxt(left_n, left_q, cur_n, cur_q, right_n, right_q, quantile, scale):
-    # calculate desired position
-    d = int(scale * quantile + 1 - cur_n)
-    if d:
-        d = copysign(1, d)  # clamp d at +/- 1
-        if left_n < cur_n + d < right_n:  # try parabolic eqn
-            nxt_q = cur_q + (d / (right_n - left_n)) * (
-                (cur_n - left_n + d) * (right_q - cur_q) / (right_n - cur_n) +
-                (right_n - cur_n - d) * (cur_q - left_q) / (cur_n - left_n))
-            if not (left_q < nxt_q < right_q):  # fall back on linear eqn
-                if d == 1:
-                    nxt_q = cur_q + (right_q - cur_q) / (right_n - cur_n)
-                elif d == -1:
-                    nxt_q = cur_q - (left_q - cur_q) / (left_n - cur_n)
-            return nxt_q, cur_n + d
-    return cur_q, cur_n
+        pass
 
 
 def test_random():
@@ -241,25 +252,37 @@ def test_random():
     # so 50th percentils ie 0.5, etc
     import random
     import time
-    nsamples = 10000
+    nsamples = 100000
     vals = [random.random() for i in range(nsamples)]
-    m = P2Accumulator()
     try:
         start = time.time()
-        for e in vals:
-            m.add_val(e)
+        #m = P2QuantileAccumulator()  # DEFAULT_PERCENTILES, vals)
+        #for val in sorted(vals):
+        #    m.add(val)
+        m = P2Estimator(DEFAULT_PERCENTILES, sorted(vals))
+        p = m.get_quantiles()
         duration = time.time() - start
-        tmpl = "processed %d measurements in %f seconds (%f ms each)"
+        tmpl = "P2QA processed %d measurements in %f seconds (%f ms each)"
         print tmpl % (nsamples, duration, 1000 * duration / nsamples)
     except:
         import traceback
         import pdb
         traceback.print_exc()
         pdb.post_mortem()
-    p = m.get_quantiles()
+        raise
     for k, v in p.items():
-        if 0.9 > (k / 100.0) / v > 1.1:
+        if 0.99 > (k / 100.0) / v > 1.01:
             print "problem: %s is %s, should be %s" % (k, v, k / 100.0)
+
+    start = time.time()
+    qa = QuantileAccumulator()
+    for val in vals:
+        qa.add(val)
+    qa.get_quantiles()
+    duration = time.time() - start
+    tmpl = "QA processed %d measurements in %f seconds (%f ms each)"
+    print tmpl % (nsamples, duration, 1000 * duration / nsamples)
+
     return m
 
 
