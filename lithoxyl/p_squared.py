c4936fd6a28ebc73117f415faadefdd07493cea9
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-06-22 23:32:21 -0700

    a bit of cleanup to get things started

--- lithoxyl/p_squared.py
+++ lithoxyl/p_squared.py
@@ -19,27 +19,7 @@ DEFAULT_PERCENTILES = (0.1, 1, 2, 5, 10, 25, 50,
                        99, 99.5, 99.8, 99.9, 99.99)
 
 
-class Stats(object):
-    def __init__(self, percentiles=DEFAULT_PERCENTILES):
-        self.percentiles = percentiles
-
-    def __getitem__(self, key):
-        if key in self.measures:
-            return self.measures[key].get_percentiles()
-        return dict([(p, None) for p in self.percentiles])
-
-    def __setitem__(self, key, val):
-        if key in self.measures:
-            self.measures[key].add_val(val)
-        self.measures[key] = Measure(self.percentiles, val)
-
-    def __repr__(self):
-        avgs = dict([(k, v.sum / v.count) for
-                     k, v in self.measures.items()])
-        return "Stats(%r)" % avgs
-
-
-class Measure(object):
+class P2Accumulator(object):
     """
     TODO
     ----
