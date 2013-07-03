f1c7df70545da1f98bb47b997614879ac2ef1ca2
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-07-02 22:26:03 -0700

    add Callpoint tests

--- lithoxyl/tests/test_basic.py
+++ lithoxyl/tests/test_basic.py
@@ -10,6 +10,7 @@ def do_debug_trans(logger):
     with logger.debug('hi') as t:
         time.sleep(0.01)
         t.success('yay')
+    return t
 
 
 def test_logger_success(trans_count=2):
@@ -25,3 +26,13 @@ def test_structured(trans_count=5):
     log = BaseLogger('test_logger', [acc])
     for i in range(trans_count):
         do_debug_trans(log)
+
+
+def test_callpoint_info():
+    log = BaseLogger('test_logger', [])
+    t = do_debug_trans(log)
+    assert t.callpoint.module_name == __name__
+    assert t.callpoint.module_path.endswith(__file__)
+    assert t.callpoint.func_name == 'do_debug_trans'
+    assert t.callpoint.lineno > 0
+    assert t.callpoint.lasti > 0
