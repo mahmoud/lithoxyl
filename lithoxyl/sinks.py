701da308273b69ca2aaacb899bc774016d251400
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-07-20 19:03:37 -0700

    first real pass at the sensible sink. lookin good so far.

--- lithoxyl/sinks.py
+++ lithoxyl/sinks.py
@@ -3,16 +3,8 @@
 import sys
 import json
 
-"""
-* Make a "classic" Handler-pattern Sink (filter + format string + emit)
-
-Format String TODOs:
-
-* Should compound field names (e.g., {x[y]}) be supported?
-* Bolton to autoconvert anonymous positional args to named positional
-  args and allow mixing (not to be encouraged though)
-
-"""
+from filters import ThresholdFilter
+from formatters import Formatter
 
 
 class AggSink(object):
@@ -42,3 +34,39 @@ class StructuredFileSink(object):
         json_str = json.dumps(msg_data, sort_keys=True)
         self.fileobj.write(json_str)
         self.fileobj.write('\n')
+
+
+def tmp_emitter(entry):
+    print entry
+
+
+class SensibleSink(object):
+    def __init__(self, filters=None, formatter=None, emitter=None):
+        self.filters = list(filters or [])
+        self.formatter = formatter
+        self.emitter = emitter
+
+    def handle(self, record):
+        if self.filters and not all([f(record) for f in self.filters]):
+            return
+        entry = self.formatter(record)
+        return self.emitter(entry)
+
+
+if __name__ == '__main__':
+    fmtr = Formatter('{start_timestamp} - {record_status}')
+    ss = SensibleSink(formatter=fmtr, emitter=tmp_emitter)
+    from logger import BaseLogger
+    log = BaseLogger('test_ss', [ss])
+    with log.debug('hi_task') as t:
+        t.warn('everything ok?')
+        t.success('doin great')
+
+
+
+"""
+Vocabangst:
+
+* Start: begin, open, init
+* Stop: finish, complete, close, (commit?)
+"""
