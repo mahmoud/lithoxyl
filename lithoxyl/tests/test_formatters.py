83c99ce19ab5815e8466334738c496db9512ca0e
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-07-21 19:43:25 -0700

    slight expansion of Formatter tests for individual fields.

--- lithoxyl/tests/test_formatters.py
+++ lithoxyl/tests/test_formatters.py
@@ -7,8 +7,28 @@ template = ('{start_timestamp} - {start_local_iso8601} - {start_iso8601}'
             ' - {logger_name} - {record_status} - {record_name}')
 
 
+RECS = [Record('Riker', DEBUG).success('Hello, Thomas.')]
+
+
+TCS = [[('{logger_name}', '""'),
+        ('{record_status}', 'success'),
+        ('{level_number}', '20'),
+        ('{record_name}', '"Riker"'),
+        ('{message}', '"Hello, Thomas."')]
+       ]
+
+
 def test_formatter_basic():
     riker = Record('hello_thomas', DEBUG).success('')
     forming = Formatter(template)
     output = forming.format_record(riker)
     assert output.endswith('"" - success - "hello_thomas"')
+
+
+def test_individual_fields():
+    for record, field_pairs in zip(RECS, TCS):
+        for field_tmpl, result in field_pairs:
+            forming = Formatter(field_tmpl)
+            output = forming.format_record(record)
+            assert output == result
+    return
