7b480374ce550b0fc96b5c265324985e362e3662
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-08-18 12:02:11 -0700

    disable quoting for the status character. this will be a fixed-width field in the future.

--- lithoxyl/formatters.py
+++ lithoxyl/formatters.py
@@ -52,7 +52,7 @@ FMT_BUILTINS = [FF('logger_name', 's', lambda r: r.logger.name),
                 FF('record_name', 's', lambda r: r.name),
                 FF('record_id', 'd', lambda r: id(r)),  # TODO
                 FF('record_status', 's', lambda r: r.status, quote=False),
-                FF('record_status_char', 's', lambda r: r.status_char),
+                FF('record_status_char', 's', lambda r: r.status_char, quote=False),
                 FF('level_name', 's', lambda r: r.level),  # TODO
                 FF('level_number', 'd', lambda r: r.level),
                 FF('message', 's', lambda r: r.message),
