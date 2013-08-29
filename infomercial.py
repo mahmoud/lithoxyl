d7818eb845fe3a66aa14ef5abb363fb52a6db07e
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-08-29 00:07:04 -0700

    add a prop script for showing basic lithoxyl usage

---
+++ infomercial.py
@@ -0,0 +1,40 @@
+"""
+"Has this ever happened to you?"
+
+Here's an example of some ostensibly well-instrumented code.
+"""
+
+import logging
+
+def create_user(name):
+    logging.info('creating user with name %r', name)
+    try:
+        success = _create_user()
+        if success:
+            logging.info('successfully created user %r', name)
+        else:
+            logging.error('failed to create user %r', name)
+    except:
+        logging.critical('exception encountered while creating user %r',
+                         name, exc_info=True)
+    return success
+
+
+"""
+Notice how the logging statements tend to dominate the code, almost
+drowning out the meaning of the code.
+
+Here's lithoxyl's take
+"""
+
+from lithoxyl import stderr_log
+
+
+def create_user(name):
+    with stderr_log.critical('user creation', reraise=False) as r:
+        success = _create_user()
+        if not success:
+            r.fail()
+
+
+_create_user = lambda: 'pretend like we created a user, ok?'
