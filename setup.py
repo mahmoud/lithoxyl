2f6321a96079f3314380f0ed3dad7c9b7213c374
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-03-06 13:08:53 -0800

    bumping for 0.2.2dev

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '0.2.1'
+__version__ = '0.2.2dev'
 __contact__ = 'mahmoudrhashemi@gmail.com'
 __url__ = 'https://github.com/mahmoud/lithoxyl'
 __license__ = 'BSD'
