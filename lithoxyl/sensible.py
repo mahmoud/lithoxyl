2f22f0d6be06e96cc85f98edde0407777073d1a6
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-03-19 18:00:46 -0700

    put fields in with sensible, since the fields are really only used by SensibleFormatter and future formatters might have their own field conventions

--- lithoxyl/sensible.py
+++ lithoxyl/sensible.py
@@ -2,18 +2,35 @@
 """Implements types and functions for rendering
 :class:`~lithoxyl.record.Record` instances into strings.
 """
-import json
 
+import os
+import time
+import json
+import datetime
+
+from boltons.timeutils import UTC, LocalTZ
+from boltons.formatutils import BaseFormatField
 from boltons.formatutils import tokenize_format_str
 
-from lithoxyl.common import EVENTS, MAX_LEVEL, get_level
-from lithoxyl.fields import BUILTIN_FIELD_MAP
+from lithoxyl.common import EVENTS, IMPORT_TIME, MAX_LEVEL, get_level
 
 
 __all__ = ['SensibleFormatter', 'SensibleSink']
 
 
 DEFAULT_QUOTER = json.dumps
+
+FIELD_MAP = {}
+BUILTIN_FIELD_MAP = {}  # populated below
+
+
+def register_builtin_field(field):
+    register_field(field)
+    BUILTIN_FIELD_MAP[field.fname] = field
+
+
+def register_field(field):
+    FIELD_MAP[field.fname] = field
 
 
 class SensibleSink(object):
@@ -311,3 +328,183 @@ Higher verbosity moves the spread of "log" actions diagonally up and
 to the left, and lower verbosity, down and to the right.
 
 """
+
+# Fields follow
+
+"""Lithoxyl comes with many built-in format *fields* meant to be used
+with the standard :class:`~lithoxyl.logger.Logger` and
+:class:`~lithoxyl.record.Record`. Sinks can take advantage of these
+with the :class:`~lithoxyl.sensible.SensibleFormatter` type or subtypes.
+"""
+# NOTE: docstring table needs slashes double escaped. Also, newline
+# literals "\n" removed.
+
+# TODO: exc_repr field
+
+
+def timestamp2iso8601_noms(timestamp, local=False, with_tz=True):
+    """
+    with time.strftime(), one would have to do fractional
+    seconds/milliseconds manually, because the timetuple used doesn't
+    include data necessary to support the %f flag.
+
+    This function is about twice as fast as datetime.strftime(),
+    however. That's nothing compared to time.time()
+    vs. datetime.now(), which is two orders of magnitude faster.
+    """
+    if with_tz:
+        tformat = '%Y-%m-%dT%H:%M:%S %Z'
+    else:
+        tformat = '%Y-%m-%dT%H:%M:%S'
+    if local:
+        tstruct = time.localtime(timestamp)
+    else:
+        tstruct = time.gmtime(timestamp)
+    return time.strftime(tformat, tstruct)
+
+
+def timestamp2iso8601(timestamp, local=False, with_tz=True, tformat=None):
+    if with_tz:
+        tformat = tformat or '%Y-%m-%dT%H:%M:%S.%f%z'
+    else:
+        tformat = tformat or '%Y-%m-%dT%H:%M:%S.%f'
+    if local:
+        dt = datetime.datetime.fromtimestamp(timestamp, tz=LocalTZ)
+    else:
+        dt = datetime.datetime.fromtimestamp(timestamp, tz=UTC)
+    return dt.strftime(tformat)
+
+
+class SensibleField(BaseFormatField):
+    """Fields specify whether or not they should be *quoted* (i.e.,
+    whether or not values will contain whitespace or other
+    delimiters), but not the exact method for their quoting. That
+    aspect is reserved for the Formatter.
+    """
+    def __init__(self, fname, fspec='s', getter=None, **kwargs):
+        quote = kwargs.pop('quote', None)
+        if kwargs:
+            raise TypeError('unexpected keyword arguments: %r' % kwargs.keys())
+        super(SensibleField, self).__init__(fname, fspec)
+        self.getter = getter
+        if quote is None:
+            # numeric fields should appear without quotes
+            numeric = issubclass(self.type_func, (int, float))
+            quote = not numeric
+        self.quote = quote
+
+
+def duration_auto(event):
+    duration = event.record.duration
+    if duration < 0.001:
+        return '%.3fus' % (duration * 1e6)
+    if duration < 1.0:
+        return '%.3fms' % (duration * 1e3)
+    return '%.4fs' % duration
+
+
+# default, fmt_specs
+_SF = SensibleField
+BASIC_FIELDS = [_SF('logger_name', 's', lambda e: e.record.logger.name),
+                _SF('logger_id', 'd', lambda e: e.record.logger.logger_id),
+                _SF('record_name', 's', lambda e: e.record.name),
+                _SF('record_id', 'd', lambda e: e.record_id),
+                _SF('status_str', 's', lambda e: e.status, quote=False),
+                _SF('status_char', 's', lambda e: e.status_char, quote=False),
+                _SF('warn_char', 's', lambda e: e.warn_char, quote=False),
+                _SF('level_name', 's', lambda e: e.level_name, quote=False),
+                _SF('data_map', 's', lambda e: json.dumps(e.record.data_map, sort_keys=True), quote=False),
+                _SF('level_name_upper', 's', lambda e: e.level_name.upper(), quote=False),
+                _SF('level_char', 's', lambda e: e.level_name.upper()[0], quote=False),
+                _SF('level_number', 'd', lambda e: e.level._value),
+                _SF('begin_message', 's', lambda e: e.begin_event.message),
+                _SF('begin_raw_message', 's', lambda e: e.begin_event.raw_message),
+                _SF('end_message', 's', lambda e: e.record.end_event.message),
+                _SF('end_raw_message', 's', lambda e: e.record.end_event.raw_message),
+                _SF('begin_timestamp', '.14g', lambda e: e.record.begin_time),
+                _SF('end_timestamp', '.14g', lambda e: e.record.end_time),
+                _SF('duration_s', '.3f', lambda e: e.record.duration),
+                _SF('duration_ms', '.3f', lambda e: e.record.duration * 1e3),
+                _SF('duration_us', '.3f', lambda e: e.record.duration * 1e6),
+                _SF('duration_auto', '>9s', duration_auto, quote=False),
+                _SF('module_name', 's', lambda e: e.callpoint.module_name),
+                _SF('module_path', 's', lambda e: e.callpoint.module_path),
+                _SF('func_name', 's', lambda e: e.callpoint.func_name, quote=False),
+                _SF('line_number', 'd', lambda e: e.callpoint.lineno),
+                _SF('exc_type', 's', lambda e: e.exc_info.exc_type, quote=False),
+                _SF('exc_message', 's', lambda e: e.exc_info.exc_msg),
+                _SF('exc_tb_str', 's', lambda e: str(e.exc_info.tb_info)),
+                _SF('exc_tb_list', 's', lambda e: e.exc_info.tb_info.frames),
+                _SF('process_id', 'd', lambda e: os.getpid())]
+
+# ISO8601 and variants. combinations of:
+#   * begin/end
+#   * UTC/Local
+#   * with/without milliseconds
+#   * with/without timezone (_noms variants have textual timezone)
+ISO8601_FIELDS = [
+    _SF('iso_begin', 's',
+        lambda e: timestamp2iso8601(e.record.begin_event.etime)),
+    _SF('iso_end', 's',
+        lambda e: timestamp2iso8601(e.record.end_event.etime)),
+    _SF('iso_begin_notz', 's',
+        lambda e: timestamp2iso8601(e.record.begin_event.etime,
+                                    with_tz=False)),
+    _SF('iso_end_notz', 's',
+        lambda e: timestamp2iso8601(e.record.end_event.etime,
+                                    with_tz=False)),
+    _SF('iso_begin_local', 's',
+        lambda e: timestamp2iso8601(e.record.begin_event.etime,
+                                    local=True)),
+    _SF('iso_end_local', 's',
+        lambda e: timestamp2iso8601(e.record.end_event.etime,
+                                    local=True)),
+    _SF('iso_begin_local_notz', 's',
+        lambda e: timestamp2iso8601(e.record.begin_event.etime,
+                                    local=True, with_tz=False)),
+    _SF('iso_end_local_notz', 's',
+        lambda e: timestamp2iso8601(e.record.end_event.etime,
+                                    local=True, with_tz=False)),
+    _SF('iso_begin_local_noms', 's',
+        lambda e: timestamp2iso8601_noms(e.record.begin_event.etime,
+                                         local=True)),
+    _SF('iso_end_local_noms', 's',
+        lambda e: timestamp2iso8601_noms(e.record.end_event.etime,
+                                         local=True)),
+    _SF('iso_begin_local_noms_notz', 's',
+        lambda e: timestamp2iso8601_noms(e.record.begin_event.etime,
+                                         local=True, with_tz=False)),
+    _SF('iso_end_local_noms_notz', 's',
+        lambda e: timestamp2iso8601_noms(e.record.end_event.etime,
+                                         local=True, with_tz=False))]
+
+# using the T separator means no whitespace and thus no quoting
+for f in ISO8601_FIELDS:
+    f.quote = False
+
+
+DELTA_FIELDS = [
+    _SF('import_delta_s', '0.6f', lambda e: e.etime - IMPORT_TIME),
+    _SF('import_delta_ms', '0.4f', lambda e: (e.etime - IMPORT_TIME) * 1000)]
+
+
+PARENT_DEPTH_INDENT = '   '
+
+
+PARENT_FIELDS = [
+    _SF('parent_depth', 'd', lambda e: e.record.parent_depth),
+    _SF('parent_depth_indent', 's',
+        lambda e: e.record.parent_depth * PARENT_DEPTH_INDENT,
+        quote=False)]
+
+
+for f in BASIC_FIELDS:
+    register_builtin_field(f)
+for f in PARENT_FIELDS:
+    register_builtin_field(f)
+for f in ISO8601_FIELDS:
+    register_builtin_field(f)
+for f in DELTA_FIELDS:
+    register_builtin_field(f)
+
+del f
