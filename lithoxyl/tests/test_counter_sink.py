a553875d1e595fd6331cc5ca899a82224fdb10d1
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-04-06 00:58:57 -0700

    adding a basic CounterSink which utilizes the boltons ThresholdCounter for bounded counting

---
+++ lithoxyl/tests/test_counter_sink.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from lithoxyl import Logger
+from lithoxyl.sinks import CounterSink
+
+
+def test_basic_counter():
+    csink = CounterSink()
+    log = Logger('ctr_log', [csink])
+    log.debug('t').success('est')
+
+    assert csink.counter_map[log]['t'] == 1
+
+    for i in range(1000):
+        log.debug('e').success('st')
+
+    assert csink.counter_map[log]['e'] == 1000
+    assert csink.counter_map[log].get('t') == 0
+
+    cdict = csink.to_dict()
+    assert cdict == {'ctr_log': {'e': 1000,
+                                 '__all__': 1001,
+                                 '__missing__': 1}}
+    return
