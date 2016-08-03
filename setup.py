ab41e580706093213015829372cd6b377d09b7ae
Mahmoud Hashemi <mahmoud@hatnote.com>
2016-08-03 00:12:51 -0700

    bumping version for v0.3.0 release

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-"""
-A systematic approach to logging, profiling, and statistics
-collection. Very lightweight, very Pythonic.
+"""A systematic approach to application instrumentation, including
+logging, semantic profiling, and statistics collection. Very
+lightweight, very Pythonic.
 
-(c) 2015 by Mahmoud Hashemi.
+(c) 2016 by Mahmoud Hashemi.
 BSD-licensed, see LICENSE for more details.
+
 """
 
 import sys
@@ -11,7 +12,7 @@ from setuptools import setup, find_packages
 
 
 __author__ = 'Mahmoud Hashemi'
-__version__ = '0.2.2dev'
+__version__ = '0.3.0'
 __contact__ = 'mahmoudrhashemi@gmail.com'
 __url__ = 'https://github.com/mahmoud/lithoxyl'
 __license__ = 'BSD'
