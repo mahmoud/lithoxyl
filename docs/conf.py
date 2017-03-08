6d84c633a21524e78371bdb094a54548ec65c0ce
Mahmoud Hashemi <mahmoud@hatnote.com>
2017-03-07 16:03:31 -0800

    bump docs version to 0.4.1

--- docs/conf.py
+++ docs/conf.py
@@ -47,9 +47,9 @@ author = u'Mahmoud Hashemi'
 
 # |version| and |release|
 # The short X.Y version.
-version = '0.1'
+version = '0.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.1.0'
+release = '0.4.1'
 
 if os.name != 'nt':
     today_fmt = '%B %e, %Y'
